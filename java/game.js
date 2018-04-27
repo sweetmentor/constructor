f5134e4b041d894a40fe7da4b05581c021a77519
Stephen Afolabi <stephenafolabi@gmail.com>
2018-04-27 12:55:04 +0000

    add changes

--- java/game.js
+++ java/game.js
@@ -3,7 +3,26 @@ let scoreBox = document.getElementById("score");
 let questionBox = document.getElementById("question");
 let answerForm = document.getElementById("quizForm");
 
+function setAdditionGame() {
+    answerForm.setAttribute("data-gametype", "addition"); 
+    additionQuiz();
+}
+function setSubtractionGame() {
+    answerForm.setAttribute("data-gametype", "subtraction"); 
+    subtractionQuiz();
+}
+function setMultiplicationGame() {
+    answerForm.setAttribute("data-gametype", "multiplication"); 
+    multiplicationQuiz();
+}
+function setDivisionGame() {
+    answerForm.setAttribute("data-gametype", "division"); 
+    divisionQuiz();
+}
+
+
 function checkAnswer() {
+    let gametype = answerForm.getAttribute("data-gametype");
     if (answerForm["answer"].value == answerForm["rightAnswer"].value) {
         alert("Hey! You got it right!");
         score++;
@@ -12,10 +31,18 @@ function checkAnswer() {
     }
     answerForm["answer"].value = "";
     scoreBox.textContent = score;
+    if (gametype == "addition") {
     additionQuiz();
+    } else if (gametype == "subtraction") {
+     subtractionQuiz();   
+    } else if (gametype == "multiplication") {
+        multiplicationQuiz();
+    } else if (gametype == "division") {
+        divisionQuiz();
+    
     return false;
 }
-
+}
 function additionQuiz() {
     let num1 = Math.floor(Math.random() * 50);
     let num2 = Math.floor(Math.random() * 50);
@@ -23,4 +50,29 @@ function additionQuiz() {
     questionBox.textContent = "What is: " + num1 + " + " + num2 + " ? ";
     answerForm["rightAnswer"].value = (num1 + num2);
 }
+
+
+function subtractionQuiz() {
+    let num1 = Math.floor(Math.random() * 50);
+    let num2 = Math.floor(Math.random() * 50);
+    
+    questionBox.textContent = "What is: " + num1 + " - " + num2 + " ? ";
+    answerForm["rightAnswer"].value = (num1 - num2);
+}
+
+function multiplicationQuiz() {
+    let num1 = Math.floor(Math.random() * 50);
+    let num2 = Math.floor(Math.random() * 50);
+    
+    questionBox.textContent = "What is: " + num1 + " * " + num2 + " ? ";
+    answerForm["rightAnswer"].value = (num1 * num2);
+}
+function divisionQuiz() {
+    let num1 = Math.floor(Math.random() * 50);
+    let num2 = Math.floor(Math.random() * 50);
+    
+    questionBox.textContent = "What is: " + num1 + " / " + num2 + " ? ";
+    answerForm["rightAnswer"].value = (num1 / num2);
+}
+
 additionQuiz();
